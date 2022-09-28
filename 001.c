ba241a68b125bbeb5528dd499b3d35504dd1e16f
YashRuhela <ruhelayash26@gmail.com>
2022-09-28 20:16:07 +0000

    area of triangle

--- 001.c
+++ 001.c
@@ -16,6 +16,6 @@ void main()
   printf("area of the triangle using formula\n");
   printf("at = sqrt(s*(s-a)*(s-b)*(s-c)) = %.2f\n",at);
 }
-// to run this code in linux, the command is 
-// gcc 1.c -lm | gcc 1.c -o output -lm -> to compile
-// ./a.out -> to run
+/* to run this code in linux, the command is 
+ * gcc 1.c -lm | gcc 1.c -o output -lm -> to compile
+ * ./a.out -> to run */
