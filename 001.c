d512e6f13580f1d2f6bfed1b6ef9027289fa255e
YashRuhela <ruhelayash26@gmail.com>
2022-09-28 18:32:40 +0000

    Program 1 and 2 of Lab1

---
+++ 001.c
@@ -0,0 +1,21 @@
+/*
+ * write a program to calculate the area of a triangle using the formula
+ * at=sqrt(s(s-a)(s-b)(s-c))
+ * */
+#include<stdio.h>
+#include<math.h>
+void main()
+{
+  int a,b,c,s;
+  float at;
+  printf("Enter the value of s = "); scanf("%d",&s);
+  printf("Enter the value of a = "); scanf("%d",&a);
+  printf("Enter the value of b = "); scanf("%d",&b);
+  printf("Enter the value of c = "); scanf("%d",&c);
+  at = sqrt(s*(s-a)*(s-b)*(s-c));
+  printf("area of the triangle using formula\n");
+  printf("at = sqrt(s*(s-a)*(s-b)*(s-c)) = %.2f\n",at);
+}
+// to run this code in linux, the command is 
+// gcc 1.c -lm | gcc 1.c -o output -lm -> to compile
+// ./a.out -> to run
---
+++ 002.c
@@ -0,0 +1,19 @@
+/*
+ * basic salary of an employee is input through the keyboard.
+ * The DA is 25% of the basic salary while HRA is 15% of the salary.
+ * provident fund is deducted at the rate of 10% of the gross salary
+ * (BS+DA+HRA).
+ * Program to calculate net salary.)*/
+#include<stdio.h>
+void main()
+{
+  float bs,da,hra,gross,pf,net;
+  printf("Enter basic salary of the Employee : "); scanf("%f",&bs);
+  da = 0.25f * bs;
+  hra = 0.15f * bs;
+  gross = bs+hra+da;
+  pf = 0.10 * gross;
+  net = bs - pf;
+  printf("DA = %.2f\nHRA = %.2f\nGROSS = %.2f\nPF = %.2f\n",da,hra,gross,pf);
+  printf("Net salary = %.2f",net);
+}
